30a3eced9cbe8bf51a52b718f4b668eff00fe300
Thuany26 <thuanydesouza26@gmail.com>
2022-10-17 19:57:43 -0300

    firts commit

---
+++ src/components/Text/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Styled from 'styled-components'
+
+const TextComponent = (text) => {
+return (
+    <>
+        <div>
+            {text}
+        </div>
+    </>
+)
+}
+
+export default TextComponent;
