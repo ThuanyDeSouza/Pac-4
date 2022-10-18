d791a7c1190f15987566b74b53cf56a20bb4f1a7
Thuany26 <thuanydesouza26@gmail.com>
2022-10-17 22:08:54 -0300

    Primeira tela e inicio da segunda

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,17 +1,30 @@
 import Styled from "styled-components";
 import TextComponent from "../../components/Text";
 import "./styles.scss"
-
+import catolica from "../../assets/images/catolica.png"
 
 const Home = () => {
     return (
         <div className="Home">
+            <img src={catolica} alt="" />
             <div style={{flex: 10}}/>
             <div style={{flex: 6, background: "#1E1E1E", flexDirection:"column"}}>
-                <div style={{flex: 3}}>
+                <div style={{flex: 1}}>
+                    <text className="texto1">Welcome back!</text>
                 </div>
-                <div style={{flex: 2, backgroundColor: "#f00"}}></div>
-                <div style={{flex: 1, backgroundColor: "#0f0"}}></div>
+                <div style={{flex: 3}}>
+                    <text className="texto2">Login to manage your <br></br>complementary hours.</text>
+                </div>
+                <div style={{flex: 2}}>
+                    <input className="input1" type="email" id="email" name="email" placeholder="Email"></input>
+                    <input className="input2" type="password" id="password" name="password" placeholder="Senha"></input>
+                </div>
+                <div style={{flex: 1}}>
+                    <text className="texto3">login is managed by your<br></br>organization</text>
+                </div>
+                <div style={{flex: 2}}>
+                    <button>Sign in</button>
+                </div>
             </div>
         </div>
     )
--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,17 +1,11 @@
 import Styled from "styled-components";
-import TextComponent from "../../components/Text";
 import "./styles.scss"
-
 
 const Main = () => {
     return (
         <div className="Main">
-            <div style={{flex: 10, background: "#FFFFFF"}}/>
-            <div style={{flex: 6, background: "#1E1E1E", flexDirection:"column"}}>
-                <div style={{flex: 3}}>
-                </div>
-                <div style={{flex: 2, backgroundColor: "#f00"}}></div>
-                <div style={{flex: 1, backgroundColor: "#0f0"}}></div>
+            <div className="hader">
+                
             </div>
         </div>
     )
